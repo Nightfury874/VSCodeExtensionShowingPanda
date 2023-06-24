b5869f29ddbd7a111b346747e16933d9e80ecc2b
Nighfury874 <jibaba874@gmail.com>
2023-06-24 16:58:33 +0530

    showing notification in the bottom panel now

--- src/webview.js
+++ src/webview.js
@@ -3,7 +3,22 @@ panel.webview.onDidReceiveMessage((message) => {
     switch (message.type) {
         case 'embedCode':
             const embedCode = message.data;
-            // Process the embed code and take necessary actions in your extension
+            processEmbedCode(embedCode);
             break;
     }
 });
+
+function processEmbedCode(embedCode) {
+    // Here, you can perform any necessary actions with the embed code
+    // For example, you can insert the embed code into the sidebar or execute it
+
+    // Inserting the embed code into the sidebar's HTML
+    const sidebarElement = document.getElementById('sidebar');
+    sidebarElement.innerHTML = embedCode;
+
+    // Alternatively, you can execute the embed code directly
+    // eval(embedCode);
+
+    // Perform any additional actions based on the embed code
+    // For example, you can manipulate the DOM or interact with other parts of your extension
+}
